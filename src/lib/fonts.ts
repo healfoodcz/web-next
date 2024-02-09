1619573a3b8f8aa76f7de4eca0ec03572ee6d8e7
ramofx <roman.dvorovoy123@gmail.com>
2024-02-09 15:47:28 +0100

    font optimization

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,10 +1,14 @@
 import { Inter, Patua_One } from 'next/font/google'
 
 export const fontPrimary = Inter({
-  subsets: ['latin'],
+  subsets: ['latin', 'cyrillic'],
+  display: 'swap',
+  preload: true,
 })
 
 export const fontLogo = Patua_One({
   subsets: ['latin'],
   weight: '400',
+  display: 'swap',
+  preload: true,
 })
