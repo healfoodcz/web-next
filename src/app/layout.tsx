ebba4447a2aa93edc86b6b96c38bb56d8431acc0
ramofx <roman.dvorovoy123@gmail.com>
2024-05-12 04:17:35 +0200

    prepared: new favicon.(light|dark).ico, tried: make next.js to load it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,25 @@
 import { PropsWithChildren } from 'react'
 import { fontPrimary } from '@/features/Common'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  icons: {
+    icon: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        url: '/favicon.light.ico',
+        media: '(prefers-color-scheme: light)',
+      },
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        url: '/favicon.dark.png',
+        media: '(prefers-color-scheme: dark)',
+      },
+    ],
+  },
+}
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
--- src/todo.md
+++ src/todo.md
@@ -8,5 +8,8 @@ Marks:
 - (L) - low priority
 
 ## (H) Add brand's favicon.ico
+- favicon.light.ico and favicon.dark.ico should be loaded by next.js conditionally
+- current problem: is it even possible? is the choosing between light and dark icons happening on server on client? is the client pulling the icon conditionally afterwards from the client side?
 
 ## (L) Use logger for server side actions?
+
